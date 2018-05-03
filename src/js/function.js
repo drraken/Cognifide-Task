c3fe94226042489c42bafc75ca997badd8abaf93
Arkadiusz Chiluta <chiluta.arkadiusz@gmail.com>
2018-05-03 15:23:44 +0200

    Last commit before making page responsive

---
+++ src/js/function.js
@@ -0,0 +1,16 @@
+
+function validateEmail(email){
+    var re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(email);
+}
+function validate(){  
+   var email = document.getElementById('emailbox').value;     
+    
+    if(validateEmail(email)){      
+        return true;
+    }
+    else{        
+        return false;
+    }
+    
+}
--- src/js/main.js
+++ src/js/main.js
@@ -1,21 +1,66 @@
 var triangle = document.getElementById('forpassword');
-var triangle2 =document.getElementById('foremail')
-triangle.addEventListener('mouseover',function(){
-   var popup = document.getElementById('required-password');
-    popup.classList.add('show');
-});
+var triangle2 = document.getElementById('foremail');
+var popup = document.getElementById('required-password');
+var popup2 = document.getElementById('required-email');
+var emailInputbox = document.getElementById('emailbox');
+var submitButton = document.getElementById('loginbutton');
+var emailParagraph = document.getElementById('emailParagraph');
 
-triangle.addEventListener('mouseout',function(){
-   var popup = document.getElementById('required-password');
-    popup.classList.remove('show');
-});
+var triangle3 =document.getElementById('forReset');
+var popup3 = document.getElementById('required-reset');
 
-triangle2.addEventListener('mouseover',function(){
-   var popup = document.getElementById('required-email');
-    popup.classList.add('show');
-});
+function wrappingIndex() {
+    if (triangle == null || triangle2 == null || popup == null || popup2 == null || emailInputbox == null || submitButton == null || emailParagraph == null) {
 
-triangle2.addEventListener('mouseout',function(){
-   var popup = document.getElementById('required-email');
-    popup.classList.remove('show');
-});
+    } else {
+        triangle.addEventListener('mouseover', function () {
+            popup.classList.add('show');
+        });
+        triangle.addEventListener('mouseout', function () {
+            popup.classList.remove('show');
+        });
+
+        triangle2.addEventListener('mouseover', function () {
+            popup2.classList.add('show');
+        });
+        triangle2.addEventListener('mouseout', function () {
+            popup2.classList.remove('show');
+        });
+    }
+};
+function wrappingReset(){
+     if (triangle3 == null || popup3 == null){
+         
+     }
+     else{
+         triangle3.addEventListener('mouseover', function () {
+            popup3.classList.add('show');
+        });
+        triangle3.addEventListener('mouseout', function () {
+            popup3.classList.remove('show');
+        });
+     }
+}
+
+wrappingIndex();
+wrappingReset();
+
+submitButton.addEventListener('click', function (e) {
+    e.preventDefault();
+    var result = document.getElementById('email_error');
+    if (validate()) {
+        result.textContent = '';
+        emailParagraph.style.color = '#7d7d7d';
+        emailInputbox.style.border = '1px solid #7d7d7d';
+        console.log('valid');
+        location.reload();
+    } else {
+        result.textContent = 'Invalid email adress';
+        emailInputbox.style.border = '1px solid red';
+        emailParagraph.style.color = 'red';
+        result.style.color = 'red';
+        emailInputbox.focus();
+        console.log('not valid');
+    }
+
+})
